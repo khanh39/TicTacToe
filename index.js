5adc65e77f0eed6de78867b17f5ed3aa45a4491a
Hamza Khan <khanh39@mcmaster.ca>
2017-01-22 14:07:09 -0500

    added code if the winner happens to be player #2

--- index.js
+++ index.js
@@ -25,4 +25,24 @@ document.getElementById("btn7").value == "X" || document.getElementById("btn1").
 document.getElementById("btn7").value == "X" || document.getElementById("btn2").value == "X" && document.getElementById("btn5").value == "X" &&
 document.getElementById("btn8").value == "X" || document.getElementById("btn3").value == "X" && document.getElementById("btn6").value == "X" &&
 document.getElementById("btn9").value == "X"){
-   
+
+  var output = document.getElementById("outtput");
+     output.innerHTML = "<b><i>X</i></b> is the winner!"
+     playerSwitch = 1;
+     resetGame();
+      }else if(
+  document.getElementById("btn1").value == "O" && document.getElementById("btn2").value == "O" &&
+  document.getElementById("btn3").value == "O" || document.getElementById("btn4").value == "O" && document.getElementById("btn5").value == "O" &&
+  document.getElementById("btn6").value == "O" || document.getElementById("btn7").value == "O" && document.getElementById("btn8").value == "O" &&
+  document.getElementById("btn9").value == "O" || document.getElementById("btn1").value == "O" && document.getElementById("btn5").value == "O" &&
+  document.getElementById("btn9").value == "O" || document.getElementById("btn3").value == "O" && document.getElementById("btn5").value == "O" &&
+  document.getElementById("btn7").value == "O" || document.getElementById("btn1").value == "O" && document.getElementById("btn4").value == "O" &&
+  document.getElementById("btn7").value == "O" || document.getElementById("btn2").value == "O" && document.getElementById("btn5").value == "O" &&
+  document.getElementById("btn8").value == "O" || document.getElementById("btn3").value == "O" && document.getElementById("btn6").value == "O" &&
+  document.getElementById("btn9").value == "O"){
+
+     var output = document.getElementById("outtput");
+     output.innerHTML = "<b><i>O</i></b> is the winner!"
+
+     playerSwitch = 1;
+     resetGame();   
